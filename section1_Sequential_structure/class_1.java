5b3896b3307a95762957723e647e1478c0f49e09
Steve <falecomstevevaz@gmail.com>
2024-01-27 15:20:26 -0300

    Resolving a some problems

--- section1_Sequential_structure/class_1.java
+++ section1_Sequential_structure/class_1.java
@@ -1,10 +1,8 @@
 package section1_Sequential_structure;
-public class class_1 {
-	
-	public static void main(String[] args) {
-	
-	   System.out.println("HELLO JAVA");
-		
-	}
 
+public class class_1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
 }
