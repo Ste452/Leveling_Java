ca1379681630fa551ec4141fd815d86d295b8ba1
Steve <falecomstevevaz@gmail.com>
2024-02-02 21:04:18 -0300

    Class 58 solving a question from a car rental company program without an interface.

---
+++ section13_Interfaces/model/entities/Vehicle.java
@@ -0,0 +1,21 @@
+package section13_Interfaces.model.entities;
+
+public class Vehicle {
+	
+	public String model;
+	
+	public Vehicle() {}
+
+	public Vehicle(String model) {
+		super();
+		this.model = model;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+}
