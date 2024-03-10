4c5995f2621e848e82f82e6e38a1572dcc428bd7
Steve <falecomstevevaz@gmail.com>
2024-03-10 19:03:32 -0300

    Default methods

---
+++ section13_Interfaces/application/model/service/BrazilInterestService.java
@@ -0,0 +1,15 @@
+package section13_Interfaces.application.model.service;
+
+public class BrazilInterestService implements InterestService {
+
+	private double interestRate;
+
+	public BrazilInterestService(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+}
