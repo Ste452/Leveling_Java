3e0caef137abfb970ad6791834089f89bd286aee
Steve <falecomstevevaz@gmail.com>
2024-06-11 22:21:14 -0300

    class_85 data actualization

---
+++ section16_Database_Access_with_JDBC/application/class_85.java
@@ -0,0 +1,41 @@
+package section16_Database_Access_with_JDBC.application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import section16_Database_Access_with_JDBC.database.DB;
+
+public class class_85 {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					  
+					"UPDATE seller " 
+					+ "SET BaseSalary = BaseSalary + ?" 
+					+ "WHERE "
+					+ "(DepartmentId = ?)"
+				);
+			
+			st.setDouble(1, 200.00);
+			st.setInt(2, 2);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows Affected: " + rowsAffected);
+		} catch (SQLException e) {
+			
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+
+}
